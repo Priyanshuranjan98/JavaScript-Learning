4c006e380608a7710775c985f33be92484e0b58c
Priyanshuranjan98 <74656626+Priyanshuranjan98@users.noreply.github.com>
2024-06-04 16:41:38 +0000

    DataTypes in js

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+'use strict' //Newer version of js is applied.
+
+//alert("hi") //this will give error here as we are using node engine to excute the code.Not executing directly on broser.
+
+let name="Priyanshu";
+let age=26;
+let isLoggedIn=true;
+
+/*
+number
+string
+boolean
+null => standalone value
+undefined 
+symbol => unique
+*/
+console.log(typeof age); //it will give data type of variable
+console.log(typeof null); //output=object
+console.log(typeof undefined);//output=undefined
