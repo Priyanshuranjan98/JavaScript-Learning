14071626b03becd9b5f54baa8f5e5728979cf756
Priyanshuranjan98 <74656626+Priyanshuranjan98@users.noreply.github.com>
2024-06-04 17:06:02 +0000

    Conversion Operations

---
+++ 01_Basics/03_conversion_Operation.js
@@ -0,0 +1,42 @@
+// let score=500;
+// let stringScore=String(score);
+// console.log(typeof stringScore); //String
+// console.log(stringScore);// 500
+
+// let score="500";
+// let NumberScore=Number(score);
+// console.log(typeof NumberScore); //number
+// console.log(NumberScore);// 500
+
+// let score="";
+// let NumberScore=Number(score);
+// console.log(typeof NumberScore); //number
+// console.log(NumberScore);// 0
+
+// let score=null;
+// let NumberScore=Number(score);
+// console.log(typeof NumberScore); //number
+// console.log(NumberScore);// 0
+
+// let score;
+// let NumberScore=Number(score);
+// console.log(typeof NumberScore); //number
+// console.log(NumberScore);// NaN
+
+let score="500abc";
+let NumberScore=Number(score);
+console.log(typeof NumberScore); //number
+console.log(NumberScore);// NaN
+/*
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+let isLoggedIn = "priyanshu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "priyanshu" => true
+*/
