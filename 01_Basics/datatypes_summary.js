f4e8eb3087c11d1d257f33586b7be8d2a4f95355
Priyanshuranjan98 <74656626+Priyanshuranjan98@users.noreply.github.com>
2024-06-05 16:31:16 +0000

    Added datatype summary

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,21 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+//1.Array
+const users=["priyanshu","Rohan","Ravi"];
+//2.Object
+let userDetails={
+    "name":"Priyanshu",
+    "age":26
+}
+//3.function
+const myfunc=function(){
+    console.log("Hello World..");
+}
+console.log(typeof users);//object
+console.log(typeof userDetails);//object
+console.log(typeof myfunc);//function
