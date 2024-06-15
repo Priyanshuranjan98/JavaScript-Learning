48bbb6411a55e4bb0d909e541e8a5f6ce34c5faf
Priyanshuranjan98 <74656626+Priyanshuranjan98@users.noreply.github.com>
2024-06-15 17:00:24 +0000

    Numbers And maths

---
+++ 01_Basics/Numbers_Maths.js
@@ -0,0 +1,12 @@
+const score=100;
+//console.log(score);
+
+const salary=new Number(100.890);
+//console.log(salary);
+// console.log(salary.toFixed(2));
+// console.log(salary.toLocaleString('en-IN'));
+//console.log(salary.toPrecision(3));
+
+const min=1;
+const max=6;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
